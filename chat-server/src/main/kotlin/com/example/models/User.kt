14cdc65728ec8f52997fc865f885899a870cc1c8
Andrew-LC <smartandrew2003@gmail.com>
2023-08-26 21:11:51 +0530

    batman

--- chat-server/src/main/kotlin/com/example/models/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class User(val id: String, val username: String, val email: String)
-
-
-val userStorage = mutableListOf<User>()
